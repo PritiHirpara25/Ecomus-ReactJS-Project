00e069cfec3dbf7544a52e5cb134c68458f37a72
PritiHirpara25 <pritihirpara256@gmail.com>
2025-02-04 07:54:17 +0530

    Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import Layout from './Pages/Layout'
+import SimpleSlider from './Pages/Home'
+import Login from './Pages/Login'
+
+function App() {
+
+  return (
+    <>
+        <Layout/>
+    </>
+  )
+}
+
+export default App
